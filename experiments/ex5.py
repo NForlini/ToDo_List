306ee28e6533c7374301f8193fad16284c41ae9e
ninaf <ninaforlini1@gmail.com>
2025-08-09 18:17:12 -0400

    halfway through day 5 part 2

---
+++ experiments/ex5.py
@@ -0,0 +1,5 @@
+waiting_list = ["Sen","Ben","John"]
+waiting_list.sort()
+
+for index, name in enumerate(waiting_list):
+    print(f'{index + 1}. {name}')
--- main.py
+++ main.py
@@ -6,14 +6,26 @@ while True:
 
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ")+ '\n'
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo.title())
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
         case 'show':
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
             if not todos:
-                print("You don't have anything to do! Congrats!")
+               print("You don't have anything to do! Congrats!")
             else:
                 for index, item in enumerate(todos):
-                    print(f'{index + 1}. {item}')
+                   print(f'{index + 1}. {item}')
         case 'edit':
             number = int(input("Which number would you like to edit? "))
             number -= 1
